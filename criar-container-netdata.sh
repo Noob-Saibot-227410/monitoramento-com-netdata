1267dc70d872c024a49ede49d382d507bd42a985
DAVI <115899706+devops-davi@users.noreply.github.com>
2023-04-24 18:55:29 -0300

    Add files via upload

---
+++ criar-container-netdata.sh
@@ -0,0 +1,14 @@
+docker pull netdata/netdata
+
+
+docker run -d --name=netdata \
+  -p 8000:19999 \
+  -v netdataconfig:/etc/netdata \
+  -v netdatalib:/var/lib/netdata \
+  -v netdatacache:/var/cache/netdata \
+  -v /etc/passwd:/host/etc/passwd:ro \
+  -v /etc/group:/host/etc/group:ro \
+  -v /proc:/host/proc:ro \
+  -v /sys:/host/sys:ro \
+  --restart unless-stopped \
+  netdata/netdata
